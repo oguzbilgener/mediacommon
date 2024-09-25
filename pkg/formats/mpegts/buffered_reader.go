d04cafbf76d646fc1e188c5f78f9e724e1f72bfd
Oguz Bilgener <obilgener@solinkcorp.com>
2024-09-25 12:21:54 -0400

    fix: Replace odd BufferedReader with a chunked reader

--- pkg/formats/mpegts/buffered_reader.go
+++ pkg/formats/mpegts/buffered_reader.go
@@ -5,40 +5,48 @@ import (
 	"io"
 )
 
-// BufferedReader is a buffered reader optimized for MPEG-TS.
+// BufferedReader is a chunked reader optimized for MPEG-TS.
 type BufferedReader struct {
-	r         io.Reader
-	midbuf    []byte
-	midbufpos int
+	r io.Reader
+	// chunk    []byte
 }
 
 // NewBufferedReader allocates a BufferedReader.
 func NewBufferedReader(r io.Reader) *BufferedReader {
 	return &BufferedReader{
-		r:      r,
-		midbuf: make([]byte, 0, 1500),
+		r: r,
+		// chunk: make([]byte, 188),
 	}
 }
 
 // Read implements io.Reader.
 func (r *BufferedReader) Read(p []byte) (int, error) {
-	if r.midbufpos < len(r.midbuf) {
-		n := copy(p, r.midbuf[r.midbufpos:])
-		r.midbufpos += n
-		return n, nil
-	}
-
-	mn, err := r.r.Read(r.midbuf[:cap(r.midbuf)])
+	n, err := io.ReadFull(r.r, p[:188])
 	if err != nil {
-		return 0, err
+		if err == io.EOF || err == io.ErrUnexpectedEOF {
+			return 0, fmt.Errorf("received packet with size %d not multiple of 188", n)
+		}
 	}
 
-	if (mn % 188) != 0 {
-		return 0, fmt.Errorf("received packet with size %d not multiple of 188", mn)
-	}
-
-	r.midbuf = r.midbuf[:mn]
-	n := copy(p, r.midbuf)
-	r.midbufpos = n
 	return n, nil
+
+	// if r.midbufpos < len(r.midbuf) {
+	// 	n := copy(p, r.midbuf[r.midbufpos:])
+	// 	r.midbufpos += n
+	// 	return n, nil
+	// }
+
+	// mn, err := r.r.Read(r.midbuf[:cap(r.midbuf)])
+	// if err != nil {
+	// 	return 0, err
+	// }
+
+	// if (mn % 188) != 0 {
+	// 	return 0, fmt.Errorf("received packet with size %d not multiple of 188", mn)
+	// }
+
+	// r.midbuf = r.midbuf[:mn]
+	// n := copy(p, r.midbuf)
+	// r.midbufpos = n
+	// return n, nil
 }
--- pkg/formats/mpegts/buffered_reader_test.go
+++ pkg/formats/mpegts/buffered_reader_test.go
@@ -22,22 +22,29 @@ func TestBufferedReader(t *testing.T) {
 	require.Equal(t, 188, n)
 	require.Equal(t, bytes.Repeat([]byte{1}, 188), byts[:n])
 
-	require.Equal(t, 0, len(buf.Bytes()))
-
-	byts = make([]byte, 188)
 	n, err = r.Read(byts)
 	require.NoError(t, err)
 	require.Equal(t, 188, n)
 	require.Equal(t, bytes.Repeat([]byte{2}, 188), byts[:n])
+
+	n, err = r.Read(byts)
+	require.NoError(t, err)
+	require.Equal(t, 188, n)
+	require.Equal(t, bytes.Repeat([]byte{3}, 188), byts[:n])
+
+	require.Equal(t, 0, len(buf.Bytes()))
+
+	_, err = r.Read(byts)
+	require.EqualError(t, err, "received packet with size 0 not multiple of 188")
 }
 
 func TestBufferedReaderError(t *testing.T) {
 	var buf bytes.Buffer
 
-	buf.Write(bytes.Repeat([]byte{1}, 1000))
+	buf.Write(bytes.Repeat([]byte{1}, 100))
 
 	r := NewBufferedReader(&buf)
 	byts := make([]byte, 188)
 	_, err := r.Read(byts)
-	require.EqualError(t, err, "received packet with size 1000 not multiple of 188")
+	require.EqualError(t, err, "received packet with size 100 not multiple of 188")
 }
