6199691c5c317f481852cf0842dd54858bf83391
Oguz Bilgener <obilgener@solinkcorp.com>
2024-12-05 14:32:32 -0500

    add logging

--- pkg/formats/mpegts/buffered_reader.go
+++ pkg/formats/mpegts/buffered_reader.go
@@ -6,31 +6,42 @@ import (
 	"io"
 )
 
-// BufferedReader is a chunked reader optimized for MPEG-TS.
+// BufferedReader is a buffered reader optimized for MPEG-TS.
 type BufferedReader struct {
-	r io.Reader
+	r         io.Reader
+	midbuf    []byte
+	midbufpos int
 }
 
 // NewBufferedReader allocates a BufferedReader.
 func NewBufferedReader(r io.Reader) *BufferedReader {
 	return &BufferedReader{
-		r: r,
+		r:      r,
+		midbuf: make([]byte, 0, 1500),
 	}
 }
 
 // Read implements io.Reader.
 func (r *BufferedReader) Read(p []byte) (int, error) {
-	n, err := io.ReadFull(r.r, p[:188])
-	if n == 0 {
-		return 0, io.EOF
+	if r.midbufpos < len(r.midbuf) {
+		n := copy(p, r.midbuf[r.midbufpos:])
+		r.midbufpos += n
+		return n, nil
 	}
+
+	mn, err := r.r.Read(r.midbuf[:cap(r.midbuf)])
 	if err != nil {
-		if err == io.EOF || err == io.ErrUnexpectedEOF {
-			return 0, fmt.Errorf("received packet with size %d not multiple of 188", n)
-		}
+		return 0, err
 	}
 
-	fmt.Printf("Read %d bytes:\n%s\n", n, hex.Dump(p[:n]))
+	if (mn % 188) != 0 {
+		return 0, fmt.Errorf("received packet with size %d not multiple of 188", mn)
+	}
 
+	fmt.Printf("Read %d bytes:\n%s\n", mn, hex.Dump(p[:mn]))
+
+	r.midbuf = r.midbuf[:mn]
+	n := copy(p, r.midbuf)
+	r.midbufpos = n
 	return n, nil
 }
